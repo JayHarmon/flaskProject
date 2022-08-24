88b6688f0fca7678b2ddf32372ffc9dd531a7095
Jaynormous <54700262+Jaynormous@users.noreply.github.com>
2022-08-24 12:29:53 +1000

    Updated app.py, forms.py and models.py to fix User Registration & to implement User Login

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from app import db
+from app import db, login
 from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -21,21 +21,25 @@ class todo(db.Model):
     text = db.Column(db.Text)
     done = db.Column(db.Boolean)
 
-    class User(UserMixin, db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        email_address = db.Column(db.String(255), unique=True)
-        name = db.Column(db.String(255))
-        password_hash = db.Column(db.String(255))
-        user_level = db.Column(db.Integer)
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email_address = db.Column(db.String(255), unique=True)
+    name = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+    user_level = db.Column(db.Integer)
 
-        def set_password(self, password):
-            self.password_hash = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
 
-    def is_admin(self):
-        if self.user_level == 2:
-            return True
-        else:
-            return False
+def is_admin(self):
+    if self.user_level == 2:
+        return True
+    else:
+        return False
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
